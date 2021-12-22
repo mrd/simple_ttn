b15edc9fdf1826d94fb5571afc6ab7458d28af5a
Matthew Danish <mrd45@cam.ac.uk>
2021-12-22 19:14:45 +0000

    device_id cannot be unique in devlice_location table

--- manage_db.py
+++ manage_db.py
@@ -16,7 +16,7 @@ def create_tables_if_necessary(db):
     cur = db.cursor()
     # vdd is battery level remaining
     db.execute('create table if not exists readings (timestamp timestamp, device_id string, temperature float, humidity int, light int, motion int, co2 int, vdd int)')
-    db.execute('create table if not exists device_location (start timestamp default current_timestamp, finish timestamp, device_id string unique, room string, detailed_location string, private bool)')
+    db.execute('create table if not exists device_location (start timestamp default current_timestamp, finish timestamp, device_id string, room string, detailed_location string, private bool)')
 
 def main(cfg):
     db = sqlite3.connect(cfg['sqlite3_db_filename'])
